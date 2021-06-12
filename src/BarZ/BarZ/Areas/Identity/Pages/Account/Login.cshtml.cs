8be5fe6f1ccf9c90a1c397d516e5bee0d8102f9b
PetyoPetkow <Pspetkov13@gmail.com>
2021-06-12 15:31:50 +0300

    Fix Login when logged in

--- src/BarZ/BarZ/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/BarZ/BarZ/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -47,19 +47,24 @@
             public bool RememberMe { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
         {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return this.Redirect("/");
+            }
+
             if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
             }
-
             returnUrl ??= Url.Content("~/");
-
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
             ReturnUrl = returnUrl;
+
+            return this.Page();
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
